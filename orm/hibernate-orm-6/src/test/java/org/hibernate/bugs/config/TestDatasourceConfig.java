f26431b5079235c61e3ed600831b08361895af94
maciej.wyrzykowski <maciej.wyrzykowski@dbschenker.com>
2025-01-07 16:52:52 +0100

    Cleanup

--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/config/TestDatasourceConfig.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/config/TestDatasourceConfig.java
@@ -3,14 +3,10 @@ package org.hibernate.bugs.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-//import org.testcontainers.containers.PostgreSQLContainer;
-//import org.testcontainers.containers.wait.strategy.Wait;
-import org.awaitility.Awaitility;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import javax.sql.DataSource;
-import java.time.Duration;
 
 @TestConfiguration
 public class TestDatasourceConfig {
